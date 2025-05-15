a68320220c65b7d7828da72cdb47abe25fd793ed
vKenjo <blasingkennviray@gmail.com>
2025-05-16 03:17:47 +0800

    updated api routes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -16,14 +16,6 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: process.env.NEXT_PUBLIC_API_URL ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*` : 'http://localhost:8000/api/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
